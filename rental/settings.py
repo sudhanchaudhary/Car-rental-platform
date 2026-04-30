9eaecb98ee4aa8df3e95333a3bcbb4ac7cf60292
Madhusudhan Chaudhary <sawuriya007@gmail.com>
2026-04-30 21:23:17 +0545

    email secrets
    Esc
    :wq
    Enter

--- rental/settings.py
+++ rental/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sawuriya007@gmail.com'
-EMAIL_HOST_PASSWORD = 'eddr uoor pkoq jaln'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIN_HOST_PASSWORD')
 
 # django jazzmin setting
 JAZZMIN_SETTINGS = {
